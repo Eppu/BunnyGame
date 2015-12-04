7ff4f49a7ae2347c1160f904679f11ef51ae0b64
Eetu Eskelinen <edwardtehgreat@gmail.com>
2015-12-04 13:02:10 +0200

    Added death screen and functionality with Player

---
+++ Bunny Game/Assets/Scripts/DieController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DieController : MonoBehaviour
+{
+    public float growRate = 1.0f;
+
+    void Grow()
+    {
+        transform.localScale = new Vector2(growRate * 0.2f, growRate * 0.2f);
+    }
+
+    void Update()
+    {
+        Grow();
+    }
+}
---
+++ Bunny Game/Assets/Scripts/DieScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DieScript : MonoBehaviour
+{
+
+    public float targetScale = 1.0f;
+    public float shrinkSpeed = 10.0f;
+    public bool shrinking;
+    public GameObject splat;
+
+    void Update()
+    {
+        if(GameObject.Find("Player").GetComponent<Player>().isDead)
+        {
+            splat.transform.localScale = Vector3.Lerp(splat.transform.localScale,
+            new Vector3(targetScale, targetScale, targetScale), Time.deltaTime * shrinkSpeed);
+        }
+    }
+
+    
+}
