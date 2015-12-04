0108b039a1dd6cc25aa9e8e0fe915370a3b7aecb
Eetu Eskelinen <edwardtehgreat@gmail.com>
2015-12-04 11:56:34 +0200

    Changed player code

--- Bunny Game/Assets/Scripts/Player.cs
+++ Bunny Game/Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 
 	public float jumpSpeed = 100.0f;
 	public float runSpeed = 5.0f;
+    public bool isDead = false;
 
 	private Rigidbody2D rb2d;
 
@@ -17,11 +18,29 @@ public class Player : MonoBehaviour
 
 	void Update () 
 	{
-		transform.Translate (runSpeed * Time.deltaTime, 0f, 0f);
-		if(Input.GetKeyDown(KeyCode.UpArrow))
+        if (isDead == false)
+        {
+            transform.Translate(runSpeed * Time.deltaTime, 0f, 0f);
+        }
+		if(isDead == false && Input.GetKeyDown(KeyCode.UpArrow))
+          
 		{
 			rb2d.AddForce(Vector2.up * jumpSpeed);
 		}
 	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Trap")
+        {
+            isDead = true;
+        }
+        if (collision.gameObject.tag == "Goal")
+        {
+            Debug.Log("You win!");
+            isDead = true;
+
+        }
+    }
 
 }
