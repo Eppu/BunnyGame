d2a62c589df324a4298c84c2626c0f142cbe9262
Eetu Eskelinen <edwardtehgreat@gmail.com>
2015-12-04 10:31:41 +0200

    First commit
    Pushed the project to GitHub

---
+++ Bunny Game/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+
+	public float jumpSpeed = 100.0f;
+	public float runSpeed = 5.0f;
+
+	private Rigidbody2D rb2d;
+
+
+	void Start ()
+	{
+		rb2d = gameObject.GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () 
+	{
+		transform.Translate (runSpeed * Time.deltaTime, 0f, 0f);
+		if(Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			rb2d.AddForce(Vector2.up * jumpSpeed);
+		}
+	}
+
+}
